82cd4d2e84a46a75ee24390614264c6fc251b9d0
Gabriel Magni <gpmagni@gmail.com>
2019-10-01 09:49:43 -0300

    Adicionar arquivos de projeto.

---
+++ OláMundoEmVB/Module1.vb
@@ -0,0 +1,8 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Olá, Mundo!") 'Imprime Olá Mundo no terminal
+        Threading.Thread.Sleep(5000) 'Espera 5 segundos (para que seja possível ler)
+    End Sub
+
+End Module
